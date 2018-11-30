5f3957159eb6071795149167f4014f13bc710da3
Hammi <laws.lea@gmail.com>
2018-11-30 08:52:52 +0100

    Update TwitterOverIbis.py

--- TwitterOverIbis.py
+++ TwitterOverIbis.py
@@ -9,7 +9,7 @@ while var == 1:
 
     for tweet in query_tweets ("#35c3", 10,begindate=dt.date.today(),enddate = dt.date(2018, 12, 30))[:5]:
         for x in range(0, len(tweet.text)-1):
-            print(tweet.text.upper()[x:x + 24])
+            #print(tweet.text.upper()[x:x + 24])
             m.DS009(tweet.text.upper()[x:x + 24])
             time.sleep(0.3)
 
