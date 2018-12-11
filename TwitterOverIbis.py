d51a7a66b89e76248a846524bc9a1a0638b5659b
Hammi <laws.lea@gmail.com>
2018-12-11 17:02:55 +0100

    Update TwitterOverIbis.py

--- TwitterOverIbis.py
+++ TwitterOverIbis.py
@@ -1,3 +1,4 @@
+
 from twitterscraper import query_tweets
 import datetime as dt
 import ibis
@@ -9,5 +10,6 @@ while var == 1:
 
     for tweet in query_tweets ("#35c3", 1,begindate=dt.date.today(),enddate = dt.date.today() + dt.timedelta(days=1))[:5]:
         for x in range(0, len(tweet.text)-1):
-            #print(tweet.text.upper()[x:x + 24]) 
-            m.DS009 (ascii(tweet.text.upper()[x:x + 24]))
+            #print(tweet.text.upper()[x:x + 24])
+            m.DS009(tweet.text.upper()[x:x + 24])
+            time.sleep(0.4)
