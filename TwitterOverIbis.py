24526f3cf7899a6e453a8bddefaedbeeef028903
Hammi <laws.lea@gmail.com>
2018-12-11 16:43:26 +0100

    Fixed problem with special characters

--- TwitterOverIbis.py
+++ TwitterOverIbis.py
@@ -10,7 +10,4 @@ while var == 1:
     for tweet in query_tweets ("#35c3", 1,begindate=dt.date.today(),enddate = dt.date.today() + dt.timedelta(days=1))[:5]:
         for x in range(0, len(tweet.text)-1):
             #print(tweet.text.upper()[x:x + 24]) 
-            m.DS009(tweet.text.upper()[x:x + 24])
-            time.sleep(0.1)
-
-
+            m.DS009 (ascii(tweet.text.upper()[x:x + 24]))
